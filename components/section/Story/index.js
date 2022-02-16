7f787f8cbd2941fa0ccfbeec7e99719a67931eee
Arsy Ramadhani <pusaka.dk@gmail.com>
2022-02-16 20:26:25 +0700

    Fix story component

--- components/section/Story/index.js
+++ components/section/Story/index.js
@@ -1,9 +1,27 @@
 import { Box, Circle, Flex, Text } from '@chakra-ui/react';
-import React from 'react';
-import { MotionText, MText } from '../../motion';
+import { useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+import { container, item } from '../../../utils/animation';
+import { date } from '../../../utils/dateConvert';
+import { MFlex, MotionText, MText } from '../../motion';
 
 const Story = ({ data, theme }) => {
     console.log({ story: data });
+
+    const controls = useAnimation();
+    const { ref, inView } = useInView({ threshold: 0, triggerOnce: true });
+
+    useEffect(() => {
+        if (inView) {
+            controls.start('visible');
+            controls.start('show');
+        }
+        if (!inView) {
+            controls.start('hidden');
+        }
+    }, [controls, inView]);
+
     return (
         <Flex
             id='story'
@@ -11,7 +29,7 @@ const Story = ({ data, theme }) => {
             py='8'
             textColor={theme.text_dark}
             px='8'
-            gap='4'
+            gap='2'
             flexDir='column'
             alignItems='center'
             bg={theme.bg_light}
@@ -22,33 +40,75 @@ const Story = ({ data, theme }) => {
                 fontSize='2xl'
                 fontWeight='semibold'
                 fontFamily={theme.font_display}
-                textColor={theme.text_dark}>
+                textColor={theme.text_dark}
+                variants={item}>
                 Our Story
             </Text>
-            <Flex flexDir='column' bg='gray.400' w='full' minH='400px'>
+            <Flex
+                flexDir='column'
+                w='full'
+                ref={ref}
+                as={MFlex}
+                animate={controls}
+                variants={container}>
                 {data.map((val, i) => (
-                    <Flex bgColor='gray.200' w='full'>
-                        <Box bgColor='gray.300' w='8' pos='relative'>
-                            <Circle
-                                mx='auto'
-                                size='4'
-                                zIndex='40'
-                                bgColor='yellow.300'
-                            />
+                    <Flex w='full' as={MFlex} variants={item}>
+                        <Box w='8' pos='relative'>
                             <Box
                                 zIndex='10'
-                                h='full'
+                                h={i === data.length - 1 ? '4' : 'full'}
                                 marginLeft='auto'
                                 marginRight='auto'
                                 left='0'
                                 right='0'
-                                w='1'
-                                top='0'
-                                bgColor='blue.300'
+                                w='2px'
+                                top={i === 0 ? '4' : 0}
+                                bgColor={theme.text_semi}
+                                pos='absolute'
+                            />
+                            <Circle
+                                mt='4'
+                                marginLeft='auto'
+                                marginRight='auto'
+                                left='0'
+                                right='0'
+                                size='4'
+                                zIndex='40'
+                                bgColor={theme.text_dark}
                                 pos='absolute'
                             />
                         </Box>
-                        <Box h='12'>xaxa</Box>
+                        <Box
+                            bgColor={theme.text_dark}
+                            ml='1'
+                            transform='auto'
+                            rotate='45'
+                            h='4'
+                            w='4'
+                            translateY='4'
+                            translateX='2'
+                            zIndex='0'
+                        />
+                        <Box
+                            zIndex='40'
+                            flex='1'
+                            mb={i === data.length - 1 ? '0' : '4'}
+                            borderColor={theme.text_dark}
+                            bgColor={theme.bg_light}
+                            border='2px'
+                            px='4'
+                            py='2'
+                            rounded='lg'
+                            maxH='40'
+                            overflowX='scroll'>
+                            <Text
+                                fontWeight='bold'
+                                textColor={theme.text_semi}
+                                fontFamily={theme.font_display}>
+                                {date(val.date)}
+                            </Text>
+                            <Text fontSize='sm'>{val.text}</Text>
+                        </Box>
                     </Flex>
                 ))}
             </Flex>
--- utils/dateConvert.js
+++ utils/dateConvert.js
@@ -23,6 +23,54 @@ const month = [
     'December'
 ];
 
+const full = date => {
+    const d = new Date(date);
+    return (
+        day[d.getDay()] +
+        ', ' +
+        d.getDate() +
+        ' ' +
+        month[d.getMonth()] +
+        ' ' +
+        d.getFullYear()
+    );
+};
+
+const display = date => {
+    const d = new Date(date);
+    return d.getDate() + ' / ' + d.getMonth() + ' / ' + d.getFullYear();
+};
+
+const date = date => {
+    const d = new Date(date);
+    return d.getDate() + ' ' + month[d.getMonth()] + ' ' + d.getFullYear();
+};
+
+const time = (date, add = 0) => {
+    const d = new Date(date);
+    const h = (d.getHours() + add).toString();
+    const m = d.getMinutes().toString();
+    return h.padStart(2, '0') + ':' + m.padStart(2, '0') + ' WIB';
+};
+
+const calculateTimeLeft = data => {
+    let year = new Date(data).getFullYear();
+    const difference = +new Date(`10/01/${year}`) - +new Date();
+
+    let timeLeft = {};
+    if (difference > 0) {
+        timeLeft = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / 1000 / 60) % 60),
+            seconds: Math.floor((difference / 1000) % 60)
+        };
+    }
+    return timeLeft;
+};
+
+export { full, display, date, time, calculateTimeLeft };
+
 const dateConvert = {
     full: date => {
         const d = new Date(date);
