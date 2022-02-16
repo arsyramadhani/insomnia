546569dc378b01494204170fa19ecece3121424c
Arsy Ramadhani <pusaka.dk@gmail.com>
2022-02-16 20:34:14 +0700

    Fix supabaseClient

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,13 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseSR = process.env.REACT_APP_SUPABASE_SERVICE_ROLE;
-
-const REACT_APP_SUPABASE_URL = 'https://opmdapizldpzeylobaik.supabase.co';
-const REACT_APP_SUPABASE_ANON_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9wbWRhcGl6bGRwemV5bG9iYWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ0MTM4MDcsImV4cCI6MTk1OTk4OTgwN30.wrxXxtmlZCwazb-3I1QB_vWiq9Exoogp4nj_i80uPHY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseSR = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
-    REACT_APP_SUPABASE_URL,
-    REACT_APP_SUPABASE_ANON_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
